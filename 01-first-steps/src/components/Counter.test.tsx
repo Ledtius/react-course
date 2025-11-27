d7a05081d1a5eb1d01e4a38ff797c0079655319b
Calet Devia <caletdevia@gmail.com>
2025-11-27 05:57:08 -0500

    Proving the component in base of the course

--- 01-first-steps/src/components/Counter.test.tsx
+++ 01-first-steps/src/components/Counter.test.tsx
@@ -1,30 +1,9 @@
-import { describe, test, expect } from "vitest";
-import { queryAllByAltText, render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { Counter } from "./Counter";
 
-describe("Counter prove", () => {
-  test("Value of the h1", () => {
-    const { container } = render(
-      <Counter
-        initial={1}
-        step={3}
-        onChange={(value) => {
-          console.log(`Changed by: ${value}`);
-        }}
-      />
-    );
-    // screen.debug();
-    // console.log(container.innerHTML);
-
-    const h1 = container.querySelector("h1");
-    const h2 = container.querySelector("button");
-
-    // console.log(h1?.innerHTML);
-    expect(h1?.innerHTML).toBe("Counter");
-    expect(h2?.innerHTML).toBe("Increase");
-  });
-
-  test("Value of the h1 - screen", () => {
+describe("Counter", () => {
+  test("Should render with default values", () => {
     render(
       <Counter
         initial={1}
@@ -35,31 +14,6 @@ describe("Counter prove", () => {
       />
     );
 
-    // screen.debug();
-    // console.log(container.innerHTML);
-    screen.debug();
-
-    // const h1 = screen.getByRole("heading", {
-    //   level: 1,
-    // });
-
-    const h1 = screen.getByTestId("h1-id");
-
-    console.log(h1?.innerHTML);
-    expect(h1?.innerHTML).toContain("Counter");
-  });
-
-  test("Should match snapshot", () => {
-    const { container } = render(
-      <Counter
-        initial={1}
-        step={3}
-        onChange={(value) => {
-          console.log(`Changed by: ${value}`);
-        }}
-      />
-    );
-
-    expect(container).toMatchSnapshot()
+    expect(screen.getByText("Counter")).toBeDefined()
   });
 });
