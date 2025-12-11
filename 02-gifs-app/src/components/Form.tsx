04824badb2ccb15b2a741dad7386f9fb340dea40
Calet Devia <caletdevia@gmail.com>
2025-12-11 08:12:47 -0500

    Created the form and the context

--- 02-gifs-app/src/components/Form.tsx
+++ 02-gifs-app/src/components/Form.tsx
@@ -5,6 +5,20 @@ export const Form = (): JSX.Element => {
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setSearchName("");
+
+    fetch(
+      "https://api.giphy.com/v1/gifs/search?api_key=oHplZomtqbhDvoZFZn4UVzr48Cms7OBD&q=goku"
+    )
+      .then((r) => r.json())
+      .then((d) => console.log(d))
+      .catch(console.error);
+
+    // fetch(
+    //   "https://api.giphy.com/v1/gifs/translate?api_key=oHplZomtqbhDvoZFZn4UVzr48Cms7OBD&s=asdasdsa"
+    // )
+    //   .then((r) => r.json())
+    //   .then((d) => console.log(d))
+    //   .catch(console.error);
   };
 
   return (
