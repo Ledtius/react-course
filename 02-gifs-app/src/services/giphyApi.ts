2eddf98829b841650d7e3b26bc8f9f157922f36d
Calet Devia <caletdevia@gmail.com>
2025-12-14 05:51:58 -0500

    Fixed the url and the action of the API

--- 02-gifs-app/src/services/giphyApi.ts
+++ 02-gifs-app/src/services/giphyApi.ts
@@ -2,12 +2,19 @@ const API_KEY = import.meta.env.VITE_GIPHY_KEY;
 
 console.log(API_KEY);
 
-export const giphyApi = async (name: string = "goku"): Promise<T> => {
-  const data = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${name}`
-  );
+export const giphyApi = async (name: string = "goku") => {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${name}`
+    );
 
-  const response = await data;
+    console.log(name);
 
-  return response;
+    const data = await response.json();
+
+    console.log(data);
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
 };
