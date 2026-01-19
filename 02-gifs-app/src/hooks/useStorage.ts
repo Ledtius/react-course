7af2cb783cf65f352585d594cce54b9889c26f9b
Calet Devia <caletdevia@gmail.com>
2026-01-19 10:50:32 -0500

    Fixed the problem when the storageValue is extracted from the localStorage value for the first time

--- 02-gifs-app/src/hooks/useStorage.ts
+++ 02-gifs-app/src/hooks/useStorage.ts
@@ -1,3 +1,4 @@
+import { parse } from "path";
 import { useEffect, useState } from "react";
 
 export const useStorage = () => {
@@ -9,15 +10,36 @@ export const useStorage = () => {
   console.log(storageValue);
 
   useEffect(() => {
-
-    /* Si el objeto tiene un numero de claves mayor a 0 (Que tenga valor el objeto), envialo al localStorage */
+    /* Si el objeto tiene un numero de claves mayor a 0 (Que tenga valor el objeto), envialo al localStorage, le agrege la condicion de adquirir item del localStorage para que no se este creando siempre */
     if (Object.keys(storageValue).length > 0) {
-      localStorage.setItem("gifsLS", storageValue);
+      localStorage.setItem("gifsLS", JSON.stringify(storageValue));
       console.log("LocalStorage Created!");
     }
   }, [storageValue]);
 
+  /* Si el objeto tiene claves, destructuralo he imprime el valor de las urls. Creamos un array de URLs en el localStorage, movimos la instancia de gifUrls afuera para apoderlo retornar */
+
+  let gifUrls: string[] = [];
+
   console.log(Object.keys(storageValue).length);
-  if (Object.keys(storageValue).length > 0)
-    return [storageValue, setStorageValue];
+  if (Object.keys(storageValue).length > 0) {
+    const { data } = storageValue;
+    console.log(data);
+
+    data.forEach((gifObj) => {
+      const { bitly_gif_url } = gifObj;
+
+      console.log(bitly_gif_url);
+      gifUrls.push(bitly_gif_url);
+    });
+
+    console.log(gifUrls);
+
+    if (!localStorage.getItem("gifUrlsLS")) {
+      console.log("No esta el gifUrlsLS");
+      localStorage.setItem("gifUrlsLS", gifUrls);
+    }
+  }
+
+  return [storageValue, setStorageValue, gifUrls];
 };
