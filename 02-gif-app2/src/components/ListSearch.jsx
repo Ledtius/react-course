d7a2eb27f3a23161243310950b7042a56cd44e57
Calet Devia <caletdevia@gmail.com>
2026-01-28 17:41:39 -0500

    Added conditional styles and working on other things

--- 02-gif-app2/src/components/ListSearch.jsx
+++ 02-gif-app2/src/components/ListSearch.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { GiphyContext } from "../context/GiphyContext";
 import { GiphyFetch } from "../services/GiphyFetch";
@@ -6,28 +6,46 @@ import { GiphyFetch } from "../services/GiphyFetch";
 export const ListSearch = () => {
   console.log("--ListSearch--");
 
-  const { searchValueList, setValueClicked } = useContext(GiphyContext);
+  const { searchValueList, setValueClicked, searchValue } =
+    useContext(GiphyContext);
 
   console.log(searchValueList);
 
+  const [stateValueClicked, setStateValueClicked] = useState("");
+
   const handleClick = (e) => {
-    console.log(e.target.textContent);
-    setValueClicked(e.target.textContent);
+    const eventValueText = e.target.textContent;
+
+    console.log(eventValueText);
+    setValueClicked(eventValueText);
+    setStateValueClicked(eventValueText);
   };
+
+  useEffect(() => {
+    if (searchValue) setValueClicked("");
+  }, [searchValue]);
+
+  console.log(stateValueClicked);
   return (
     <>
       <h2 className="text-xl font-bold">Historial de busqueda</h2>
 
       <ul className="flex justify-center gap-3">
-        {searchValueList.map(({ value, id }) => (
-          <li
-            className="border-2 border-yellow-400 rounded-2xl text-white bg-black p-2 cursor-pointer"
-            key={id}
-            onClick={handleClick}
-          >
-            {value}
-          </li>
-        ))}
+        {searchValueList.map(({ value, id }) => {
+          return (
+            <li
+              className={`rounded-2xl p-2 cursor-pointer hover:bg-gray-500 ${
+                stateValueClicked === value
+                  ? "text-white bg-black"
+                  : "text-black bg-red-400"
+              }`}
+              key={id}
+              onClick={handleClick}
+            >
+              {value}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
